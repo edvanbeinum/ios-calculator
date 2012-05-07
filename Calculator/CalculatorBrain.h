64f72a8db40cfb096d1ae2ba56afcd22c250ca4d
Ed van Beinum <ed@vanbeinum.org>
2012-05-07 13:46:50 +0100

    first!

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Ed van Beinum on 07/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+@end
